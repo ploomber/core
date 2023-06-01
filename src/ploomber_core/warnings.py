f9521387f3229d71c6af999c88502af96d0fe3ff
Tony Kuo <yen.c@ploomber.io>
2023-06-01 08:43:23 -0400

    add simple func

--- src/ploomber_core/warnings.py
+++ src/ploomber_core/warnings.py
@@ -1,3 +1,7 @@
+from warnings import warn
+from uuid import uuid4
+import posthog
+
 class PloomberDeprecationWarning(FutureWarning):
     """
     Notes
@@ -5,5 +9,14 @@ class PloomberDeprecationWarning(FutureWarning):
     .. versionchanged:: 0.1
         Changed superclass from UserWarning to FutureWarning
     """
+    # @
+    def __init__(self, message):
+        print ("message: ", message)
+        self.message = message
 
-    pass
+def deprecation_warning(message):
+    warn(message, FutureWarning)
+    # log the event to posthog
+    posthog.project_api_key = "phc_JtG9P0pl0v0XExLqbqKfmXZjUm2wFq9cCxHE4LM74IG"
+    event_id = uuid4()
+    posthog.capture(event_id, "deprecation-warning-shown", {"message": message})
